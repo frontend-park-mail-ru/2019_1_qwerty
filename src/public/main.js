43a29060720e01757a8267c2bf8c7365a8aabc6e
KostyaBobroff <k.bobrov.jr@gmail.com>
2019-03-01 02:46:44 +0300

    Added signin spa

--- src/public/main.js
+++ src/public/main.js
@@ -2,3 +2,94 @@
 
 const application = document.getElementById('application');
 
+const menuItems = {
+    signup: "Sign Up",
+    signin: "Sign In"
+    //... Дописать самим
+};
+
+function createMenu() {
+    Object.keys(menuItems).forEach(function (key) {
+        const menuItem = document.createElement("a");
+        menuItem.textContent = menuItems[key];
+        menuItem.href = "/" + key;
+        menuItem.className = "application__item";
+        menuItem.dataset.section = key;
+
+        application.appendChild(menuItem);
+    });
+
+}
+
+//signin
+function createSignin() {
+    application.innerHTML = "";
+    const formSection = document.createElement("section");
+    formSection.classList.add(...["signin", "signin_center", "signin_text_center"]);
+
+    const signInHeader = document.createElement("h1");
+    signInHeader.className = "signin__header";
+    signInHeader.textContent = "Sign In";
+
+    const signInForm = document.createElement("form");
+    signInForm.className = "signin-form";
+    signInForm.method="post";
+    signInForm.action = "/signin";
+
+    const signInNickname = document.createElement("input");
+    signInNickname.classList.add(...["signin-form__input",  "signin-form__input_margin_m"]) ;
+    signInNickname.type = "text";
+    signInNickname.placeholder = "Nickname";
+
+    const signInDivContainer = document.createElement("div");
+    signInDivContainer.classList.add(...["signin-form__content",  "signin-form__input_margin_m"]);
+
+    const signInPassword = document.createElement("input");
+    signInPassword.classList.add(...["signin-form__input",  "signin-form__input_size_m"]);
+    signInPassword.type = "password";
+    signInPassword.dataset.section = "password";
+    signInPassword.placeholder = "Password";
+
+    const showIcon = document.createElement("i");
+    showIcon.className = "signin-form__icon";
+
+    signInDivContainer.appendChild(signInPassword);
+    signInDivContainer.appendChild(showIcon);
+
+    const signInButton = document.createElement("button");
+    signInButton.className = "signin-form__button";
+    signInButton.textContent = "Sign In";
+
+    signInForm.appendChild(signInNickname);
+    signInForm.appendChild(signInDivContainer);
+    signInForm.appendChild(signInButton);
+
+    formSection.appendChild(signInHeader);
+    formSection.appendChild(signInForm);
+
+    showIcon.addEventListener("click", function (event) {
+        signInPassword.type = signInPassword.type === "password" ? "text" : "password";
+    });
+
+    application.appendChild(formSection);
+}
+
+createMenu();
+
+const pages = {
+    menu: createMenu,
+    signin: createSignin,
+    //...
+};
+
+application.addEventListener("click", function (event) {
+   const currentTarget =  event.target;
+
+   if(currentTarget instanceof HTMLAnchorElement) {
+       event.preventDefault();
+       const section = currentTarget.dataset.section;
+       pages[section]();
+   }
+});
+//signin
+
--- src/server.js
+++ src/server.js
@@ -11,9 +11,25 @@ app.use(morgan('dev'));
 app.use(body.json());
 app.use(express.static('./src/public/'));
 
+const users = {
+    "bobroff" : {
+        email:"k@gmail.com",
+        score:"34"
+    }
+};
+
+app.post('/login', function (req, res) {
+    const nickname = req.body.nickname;
+    const password = req.body.password;
+
+    if (!nickname || !password ){
+        return
+    }
+});
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
     console.log(`server is listening on ${port}`)
-})
+});

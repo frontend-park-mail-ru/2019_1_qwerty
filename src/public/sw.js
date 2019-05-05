9ab60c2cb2800b88e1d53825f356b4d5f88adc47
Lunex08 <Lunex.08@gmail.com>
2019-05-05 19:09:46 +0300

    Deleted debug console.print

--- src/public/sw.js
+++ src/public/sw.js
@@ -8,12 +8,10 @@ addEventListener('fetch', function (event) {
             .then(function (response) {
                 // Cache hit - return response
                 var url = new URL(event.request.url);
-                console.log('trigger 1');
                 if (!navigator.onLine) {
                     if (response) {
                         return response;
                     } else if (urls.includes(url.pathname)) {
-                        console.log('trigger 2');
                         url.pathname = '/';
                         return caches.match(url).then(response => {
                             if (response) {
@@ -22,19 +20,14 @@ addEventListener('fetch', function (event) {
                                 return null;
                             }
                         });
-                    } else {
-                        console.log('BAD with: ', url.pathname);
                     }
                 }
 
                 return fetch(event.request).then(function (response) {
-                    console.log('trigger 3');
                     // Check if we received a not valid response
                     if (!response || response.status !== 200) {
-                        console.log('trigger 4');
                         return response;
                     }
-                    console.log('trigger 5');
                     let responseToCache = response.clone();
                     caches.open(CACHE_NAME)
                         .then(function (cache) {

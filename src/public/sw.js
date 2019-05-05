236ae9ec354aa08515316700e67f8011ec4dd529
Lunex08 <Lunex.08@gmail.com>
2019-05-05 18:57:50 +0300

    Added allowed urls

--- src/public/sw.js
+++ src/public/sw.js
@@ -1,15 +1,19 @@
 const CACHE_NAME = 'qwerty-cache';
 
+let urls = ['/', '/signin', '/signup', 'profile', '/score', '/singleplayer'];
+
 addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request.url)
             .then(function (response) {
-            // Cache hit - return response
+                // Cache hit - return response
+                var url = new URL(event.request.url);
+                console.log("trigger 1");
                 if (!navigator.onLine) {
                     if (response) {
                         return response;
-                    } else {
-                        var url = new URL(event.request.url);
+                    } else if (urls.includes(url.pathname)) {
+                        console.log("trigger 2");
                         url.pathname = '/';
                         return caches.match(url).then(response => {
                             if (response) {
@@ -18,15 +22,19 @@ addEventListener('fetch', function (event) {
                                 return null;
                             }
                         });
+                    } else {
+                        console.log("BAD with: ", url.pathname);
                     }
                 }
 
                 return fetch(event.request).then(function (response) {
+                    console.log("trigger 3");
                     // Check if we received a not valid response
                     if (!response || response.status !== 200) {
+                        console.log("trigger 4");
                         return response;
                     }
-
+                    console.log("trigger 5");
                     let responseToCache = response.clone();
                     caches.open(CACHE_NAME)
                         .then(function (cache) {
@@ -37,20 +45,5 @@ addEventListener('fetch', function (event) {
                 }
                 );
             })
-    );
-});
-
-this.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (resp) {
-            return resp || fetch(event.request).then(function (response) {
-                caches.open('v1').then(function (cache) {
-                    cache.put(event.request, response.clone());
-                });
-                return response;
-            });
-        }).catch(function () {
-            return caches.match('/sw-test/gallery/myLittleVader.jpg');
-        })
     );
 });
